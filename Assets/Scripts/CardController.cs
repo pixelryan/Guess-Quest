7931721e3c0c8f352aab9b29c3706015a23c6200
Ryan Dormanesh <3446287+pixelryan@users.noreply.github.com>
2023-11-22 09:49:57 -0800

    Buttons draw new cards.

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -15,25 +15,62 @@ public class CardController : MonoBehaviour
     public void SetUpNewCard(Card thisCard)
     {
         Debug.Log("Setting up a new card " + thisCard.suit + " " + thisCard.rank);
-        rank.text = thisCard.rank.ToString();
+        rank.text = RankToString(thisCard.rank);
         Color textColor;
         textColor = (thisCard.suit == Card.Suit.Hearts || thisCard.suit == Card.Suit.Diamonds) ? red : black;
         rank.color = textColor;
-        if (thisCard.suit == Card.Suit.Clubs)
+        Sprite suiteSprite = null;
+        switch (thisCard.suit)
         {
-            suiteTL.sprite = club;
+            case Card.Suit.Clubs:
+                suiteSprite = club;
+                break;
+            case Card.Suit.Diamonds:
+                suiteSprite = diamond;
+                break;
+            case Card.Suit.Hearts:
+                suiteSprite = heart;
+                break;
+            case Card.Suit.Spades:
+                suiteSprite = spade;
+                break;
         }
-        if (thisCard.suit == Card.Suit.Diamonds)
+        suiteTL.sprite = suiteSprite;
+        suiteTR.sprite = suiteSprite;
+    }
+
+    private string RankToString(Card.Rank rank)
+    {
+        switch (rank)
         {
-            suiteTL.sprite = diamond;
-        }
-        if (thisCard.suit == Card.Suit.Hearts)
-        {
-            suiteTL.sprite = heart;
-        }
-        if (thisCard.suit == Card.Suit.Spades)
-        {
-            suiteTL.sprite = spade;
+            case Card.Rank.Ace:
+                return "A";
+            case Card.Rank.Two:
+                return "2";
+            case Card.Rank.Three:
+                return "3";
+            case Card.Rank.Four:
+                return "4";
+            case Card.Rank.Five:
+                return "5";
+            case Card.Rank.Six:
+                return "6";
+            case Card.Rank.Seven:
+                return "7";
+            case Card.Rank.Eight:
+                return "8";
+            case Card.Rank.Nine:
+                return "9";
+            case Card.Rank.Ten:
+                return "10";
+            case Card.Rank.Jack:
+                return "J";
+            case Card.Rank.Queen:
+                return "Q";
+            case Card.Rank.King:
+                return "K";
+            default:
+                return rank.ToString();
         }
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
     public GameObject cardPrefab, cardSpawnLocator;
     public Deck deck;
+    private GameObject lastCardDrawn;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +22,24 @@ public class GameController : MonoBehaviour
         
     }
 
+    public void HigherButtonPress()
+    {
+        Debug.Log("Higher Pressed");
+        DealHand();
+    }
+
+    public void LowerButtonPress()
+    {
+        Debug.Log("Lower Pressed");
+        DealHand();
+    }
+
     public void DealHand()
     {
+        if (lastCardDrawn != null)
+        {
+            Destroy(lastCardDrawn);
+        }
         deck.ShuffleDeck();
         Card drawnCard = deck.DrawCard();
         if (drawnCard != null)
@@ -29,6 +47,7 @@ public class GameController : MonoBehaviour
             GameObject cardObject = Instantiate(cardPrefab, cardSpawnLocator.transform.position, Quaternion.identity, cardSpawnLocator.transform);
             CardController cardController = cardObject.GetComponent<CardController>();
             cardController.SetUpNewCard(drawnCard);
+            lastCardDrawn = cardObject;
         }
     }
 
