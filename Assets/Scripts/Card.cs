04b80a51a2b0443d93a170debc979fe733abd24b
Ryan Dormanesh <3446287+pixelryan@users.noreply.github.com>
2023-11-21 16:11:09 -0800

    Card and Deck Classes
    I've added card and deck classes to start working on the game logic.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card
+{
+    public enum Suit
+    {
+        Hearts,
+        Clubs,
+        Diamonds,
+        Spades
+    }
+
+    public enum Rank
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+
+public class Deck 
+{
+    private List<Card> cards = new List<Card>();
+
+}
